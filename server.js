e61a6bfba67d5ee246708b8771f410032ccfd762
MarkusSMadsbakken <mamaa003@osloskolen.no>
2023-02-13 23:11:27 +0100

    server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+// motta spilloppdateringer fra brukeren, og sjekke om riktig spiller har gitt input
+// inkluderer navn/ip, hvilket move
+
+// oppdatere spill-string
+
+// måter å implimentere sse
+// https://medium.com/system-design-blog/long-polling-vs-websockets-vs-server-sent-events-c43ba96df7c1
+// https://dev.to/dhiwise/how-to-implement-server-sent-events-in-nodejs-11d9
+// https://stackoverflow.com/questions/36249684/simple-way-to-implement-server-sent-events-in-node-js
+
+
+// bruke sse for å sende dataen til alle brukerene samtidig
+// kalles fra oppdaterings-funksjonen
