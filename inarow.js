a1017cea0265efee0071c2431e6eb054ba514503
erhoa002 <erhoa002@osloskolen.no>
2023-02-14 09:42:27 +0100

    Masse ting

--- inarow.js
+++ inarow.js
@@ -1,5 +1,4 @@
 let canvas = document.getElementById("rowgame")
-console.log(canvas)
 var ctx = canvas.getContext("2d");
 let ww = canvas.width
 let wh = canvas.height
@@ -7,11 +6,41 @@ const wp = 4/5
 let dim = [5,5]
 let tile_size = Math.min(ww*wp/dim[0], wh*wp/dim[1])
 let tile_color = "rgb(155, 102, 102)"
+let board = []
+for (let i = 0; i < dim[0]; i++) {
+    board.push([])
+}
+let turn = 0
+let player_info = 
+[{color: "rbg(200,50,50)",name: "Rød"},
+{color: "rbg(50,200,50)",name: "Grønn"},
+{color: "rbg(50,50,200)",name: "Blå"}]
+players = 2
 
+function c_click(event) {
+    let x = event.clientX - ww/2 + dim[0]/2*tile_size;
+    let y = event.clientY - wh/2 - dim[1]/2*tile_size;
+    let collum = Math.floor(x/tile_size)
+    place(collum)
+}
+function place(collum) {
+    if (collum >= 0 && collum < dim[0]) {
+        if (board[collum].length < dim[1]) {
+            board[collum].push(turn)
+            turn ++
+            if (turn >= players) {
+                turn = 0
+            }
+        }
+    }
+    Draw()
+}
 
 function Draw() {
-    console.log("draw")
+    //fill with background
+    console.log(board)
     ctx = canvas.getContext("2d");
+
     // draw all tiles
     for (let i = 0; i < dim[0]; i++) {
         for (let j = 0; j < dim[1]; j++) {
@@ -19,10 +48,15 @@ function Draw() {
         }
     }
     // draw all pieeeecesss
-
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++) {
+            Circle(player_info[board[i][j]].color, ww/2 - dim[0]/2*tile_size + i*tile_size + tile_size/2, wh/2 + dim[1]/2*tile_size - j*tile_size - tile_size/2, tile_size*3/8)
+        }
+    }
 }
-function Circle(x, y, r) {
+function Circle(color, x, y, r) {
     ctx.beginPath();
+    //set color
     ctx.arc(x, y, r, 0, 2 * Math.PI);
     ctx.stroke();
 }
