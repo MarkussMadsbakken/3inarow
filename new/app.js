0fda70d9ff6b3fc8407f89c918afc49e36b92016
Markus <mamaa003@osloskolen.no>
2023-05-23 14:37:28 +0200

    minor update

--- new/app.js
+++ new/app.js
@@ -1,3 +1,29 @@
-database = require("./database.js")
+const database = require("./database.js")
 
-database.initdb();
+dbpath = "./database/db.db"
+
+database.dbinit();
+
+//express init
+const port = 3000
+const express = require('express');
+const app = express();
+
+//passport init
+const passport = require('passport')
+var localStrategy = require('passport-local')
+
+passport.use(new localStrategy (db.verify(username,password)))
+
+
+
+
+//const multer = require('multer')
+//const sharp = require('sharp')
+
+//starte server
+app.listen(port)
+console.log("server started: http://localhost:"+port)
+
+//css
+app.use(express.static(__dirname + '/public'));
--- new/database.js
+++ new/database.js
@@ -1,5 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 
+const dbPath = "./database/db.db"
+
 function dbinit(){
     const db = new sqlite3.Database(dbPath)
     db.serialize(() => {
